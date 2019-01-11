461a77b96def1fd3104a4ff9043924607f5ef3f0
Risfeng <risfeng@126.com>
2019-01-12 00:59:06 +0800

    222

--- TestDevOps/Controllers/SampleDataController.cs
+++ TestDevOps/Controllers/SampleDataController.cs
@@ -10,7 +10,7 @@ namespace TestDevOps.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm11", "Balmy", "Hot", "Sweltering", "Scorching","Risfeng","Nocile"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild222", "Warm11", "Balmy", "Hot", "Sweltering", "Scorching","Risfeng","Nocile"
         };
 
         [HttpGet("[action]")]
